79c8219af67080ad27e5cc76c4a672517348c8e4
Jasper Delahaije <jdelahaije@gmail.com>
2023-03-07 09:02:18 +0100

    rebuild format

--- workspace/work.bat
+++ workspace/work.bat
@@ -2,12 +2,28 @@ set RESULTS_DIR=results
 set LINKS_FILE=links.xml
 set XSLT_FILE=xs3p.xsl
 set ORIGINAL_XSLT_FILE=..\%XSLT_FILE%
+
 @REM Copy XSLT file to local directory
 copy %ORIGINAL_XSLT_FILE% .
+
 @REM Create results directory
 if not exist %RESULTS_DIR% mkdir %RESULTS_DIR%
+
 @REM Loop through schema files
-for %%f in (*.xsd) do msxsl %%f %XSLT_FILE% -t -o %RESULTS_DIR%\%%f.html searchImportedSchemas=true searchIncludedSchemas=true linksFile=%LINKS_FILE%
+setlocal enabledelayedexpansion
+
+
+
+for /R .\xsd %%f in (*.xsd) do (
+    set subpath=%%~dpf
+    set finalpath=!subpath:%CD%\xsd=!
+
+    if not exist %RESULTS_DIR%!finalpath! mkdir %RESULTS_DIR%!finalpath!
+
+    msxsl "%%f" %XSLT_FILE% -t -o %RESULTS_DIR%!finalpath!%%~nf.html searchImportedSchemas=true searchIncludedSchemas=true linksFile=%LINKS_FILE%
+)
+endlocal
+
 @REM Delete local copy of stylesheet
 del %XSLT_FILE%
 
